dcecbd5bf90402065ad900c60b6b5e041166d5c6
zhuyingames <zhuyingames@proton.me>
2024-05-29 18:09:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log("edonjs: Editor on NodeJS");
+//console.log("edonjs: Editor on NodeJS");
+
+const fs = require("fs");
+
+const stdin = process.stdin;
+const stdout = process.stdout;
+
+stdin.setRawMode(true);
+
+stdin.on("data", (data) => {
+  const number = data[0];
+  if (number === 27) {
+    process.exit(0);
+  }
+  const ascii = String.fromCharCode(number);
+  stdout.write(ascii);
+});
