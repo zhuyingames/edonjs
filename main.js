a84a81962e7aa56c5295780aeefd9a1564694ff2
zhuyingames <zhuyingames@proton.me>
2024-05-29 19:04:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ const fs = require("fs");
 const stdin = process.stdin;
 const stdout = process.stdout;
 
+const ESC = "\u001b";
+
+function useAlternateScreenBuffer() {
+  stdout.write(`${ESC}[?1049h`);
+}
+
+function useMainScreenBuffer() {
+  stdout.write(`${ESC}[?1049l`);
+}
+
+useAlternateScreenBuffer();
+
+process.on("exit", () => {
+  useMainScreenBuffer();
+});
+
 stdin.setRawMode(true);
 
 stdin.on("data", (data) => {
