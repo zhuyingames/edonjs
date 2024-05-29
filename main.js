dc6e24d85a057a97ab3913e945b404abba7ddc7d
zhuyingames <zhuyingames@proton.me>
2024-05-30 02:45:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //console.log("edonjs: Editor on NodeJS");
 
-const VERSION = "0.0.6";
+const VERSION = "0.0.7";
 
 const fs = require("fs");
 
@@ -12,6 +12,12 @@ const stdout = process.stdout;
 const ESC = "\u001b";
 
 const inputBuffer = [];
+let rowIndex = 0;
+let columnIndex = 0;
+let cursorX = 0;
+let cursorY = 0;
+let rows = 0;
+let colunms = 0;
 
 function useAlternateScreenBuffer() {
   stdout.write(`${ESC}[?1049h`);
@@ -35,7 +41,15 @@ if (process.argv.length > 2) {
 
 useAlternateScreenBuffer();
 
-stdout.cursorTo(0, 0);
+stdout.cursorTo(cursorX, cursorY);
+
+rows = stdout.rows;
+colunms = stdout.columns;
+
+stdout.on("resize", () => {
+  rows = stdout.rows;
+  colunms = stdout.columns;
+});
 
 process.on("exit", () => {
   useMainScreenBuffer();
@@ -60,8 +74,33 @@ stdin.on("data", (data) => {
     } else if (number <= 126) {
       const ascii = String.fromCharCode(number);
       inputBuffer.push(ascii);
-      stdout.write(ascii);
+      if (columnIndex < colunms) {
+        stdout.write(ascii);
+        cursorX++;
+      } else {
+        stdout.cursorTo(0, cursorY);
+        const offsetX = 1 + columnIndex - colunms;
+        for (let i = offsetX; i < colunms + offsetX; i++) {
+          stdout.write(inputBuffer[i]);
+        }
+      }
+      columnIndex++;
     } else {
+      if (columnIndex > 0) {
+        inputBuffer.pop();
+        columnIndex--;
+        if (columnIndex < colunms) {
+          stdout.cursorTo(columnIndex, cursorY);
+          stdout.write(" ");
+          stdout.cursorTo(columnIndex, cursorY);
+        } else {
+          stdout.cursorTo(0, cursorY);
+          const offsetX = columnIndex - colunms;
+          for (let i = offsetX; i < colunms + offsetX; i++) {
+            stdout.write(inputBuffer[i]);
+          }
+        }
+      }
     }
   } else {
   }
